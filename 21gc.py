afdb39f17d237d2543bd42997e92d0c6da9655b4
5ophia5 <stmdao@ucdavis.edu>
2022-01-31 23:37:32 -0800

    done

---
+++ 21gc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the GC% of a DNA sequence
+# Format the output for 2 decimal places
+# Use all three formatting methods
+
+dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT' # feel free to change
+print(len(dna))
+
+
+"""	
+for i in range(len(dna) -1):
+	if dna[i] == 'G' and dna[i+1] == 'C':
+		n = len(dna)
+		div = i / n
+		print(
+python3 21gc.py
+0.42
+0.42
+0.42
+"""
